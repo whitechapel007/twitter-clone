157b338cb60fefe2465cbe5133b6b5f30df7a807
whitechapel007 <ebukareed@gmail.com>
2025-06-23 08:54:26 +0100

    fix:build

--- composables/usePrisma.server.ts
+++
@@ -1,9 +0,0 @@
-// Server-only composable for Prisma
-export const usePrisma = () => {
-  // This will only be available on the server side
-  if (process.server) {
-    return import("~/lib/prisma").then(m => m.default);
-  }
-  
-  throw new Error("Prisma can only be used on the server side");
-};
